578e6fa8a4915d076f41adcff10dc0305e2a9022
redtide <redtid3@gmail.com>
2018-12-22 15:44:24 +0100

    Avoid Travis to deploy untagged commit builds.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,7 +10,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     export DEPLOYFILE=SmartHHC.dmg
 else
 #   linuxdeployqt uses this for naming the file
-    export VERSION=$(git rev-parse --short HEAD)
+    export VERSION=$(git describe --tags)
     wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
     chmod a+x linuxdeployqt-continuous-x86_64.AppImage
     ./linuxdeployqt-continuous-x86_64.AppImage SmartHHC/usr/share/applications/*.desktop -appimage -no-plugins -no-strip -no-translations -verbose=2
