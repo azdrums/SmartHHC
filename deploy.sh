a64194ebcafffc429cf3d75645a06d4f61faefec
redtide <redtid3@gmail.com>
2018-12-22 14:21:48 +0100

    Typo in deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ else
     export VERSION=$(git rev-parse --short HEAD)
     wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
     chmod a+x linuxdeployqt-continuous-x86_64.AppImage
-    ./linuxdeployqt-continuous-x86_64.AppImage SmartHHC/usr/share/applications/*.desktop -appimage -no-plugins -no-strip -no-translations -verbose=2
+    linuxdeployqt-continuous-x86_64.AppImage SmartHHC/usr/share/applications/*.desktop -appimage -no-plugins -no-strip -no-translations -verbose=2
     export DEPLOYFILE=SmartHHC-$VERSION-x86_64.AppImage
 fi
