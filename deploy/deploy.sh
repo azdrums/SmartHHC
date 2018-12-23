7eaa1f1f827f516c11361b0c2912f10946c0aa6e
redtide <redtid3@gmail.com>
2018-12-23 15:47:25 +0100

    Adding OSX deployment.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,7 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 #   TODO: Add MacOS deployment
+    macdeployqt SmartHHC.app -dmg -no-plugins -no-strip -verbose=2
 #   sudo chmod +x ./tools/macdeployqt.app/Contents/MacOS/macdeployqt
 #   sudo ./tools/macdeployqt.app/Contents/MacOS/macdeployqt src/SmartHHC.app -dmg -no-plugins -no-strip -verbose=2
 #   user=$(id -nu)
