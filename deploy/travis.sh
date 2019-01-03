efe5e4f135a02bf91a2bf6be5d97d1d332a228f0
redtide <redtid3@gmail.com>
2019-01-03 14:40:44 +0100

    MacOSX deployment.

--- deploy/travis.sh
+++ deploy/travis.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+# linuxdeployqt uses this for naming the file
+export VERSION=$(git describe --tags)
+
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 #   TODO: Add MacOS deployment
-    cd ./Release
-    macdeployqt SmartHHC.app -dmg -no-plugins -no-strip -verbose=2
-    export DEPLOYFILE=SmartHHC.dmg
+    macdeployqt smarthhc.app -dmg -verbose=2
+    export DEPLOYFILE=smarthhc.dmg
 else
-#   linuxdeployqt uses this for naming the file
-    export VERSION=$(git describe --tags)
     wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
     chmod a+x linuxdeployqt-continuous-x86_64.AppImage
     ./linuxdeployqt-continuous-x86_64.AppImage SmartHHC/usr/share/applications/*.desktop -appimage -no-plugins -no-strip -verbose=2
