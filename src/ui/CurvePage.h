d2d786c29bb66d46e408fa627c6414c8b67729c6
redtide <redtid3@gmail.com>
2019-11-07 00:17:41 +0100

    Replace MPL 2.0 license with GPL 2.0.

--- src/ui/CurvePage.h
+++ src/ui/CurvePage.h
@@ -1,10 +1,20 @@
 /*
 	SmartHHC - Electronic Drums HiHat Controller Manager
-	Copyright (C) 2018 Andrea Zanellato
+	Copyright (C) 2018-2019 Andrea Zanellato
 
-	This Source Code Form is subject to the terms of the Mozilla Public
-	License, v. 2.0. If a copy of the MPL was not distributed with this
-	file, You can obtain one at http://mozilla.org/MPL/2.0/.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 #pragma once
 #include <QWidget>
