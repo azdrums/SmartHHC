6ecb0154296c3b2fb23b752e8cf835ed49733a98
redtide <redtid3@gmail.com>
2019-11-07 11:34:35 +0100

    Update Travis script.

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  source /opt/qt*/bin/qt*-env.sh
+  source /opt/qt510/bin/qt510-env.sh
   export QT_SELECT=qt5
 fi
--- .travis/install.sh
+++ .travis/install.sh
@@ -2,7 +2,8 @@
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo apt-get -y install libgl1-mesa-dev qt510base qt510serialport
-  source /opt/qt*/bin/qt*-env.sh
+  source /opt/qt510/bin/qt510-env.sh
+  export QT_SELECT=qt5
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   brew link --force qt5
   export PATH=/usr/local/opt/qt5/bin:$PATH
